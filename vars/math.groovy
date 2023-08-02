44ddce705e52a754b4f421f6fc2897946b626fef
shashd1 <140515620+shashd1@users.noreply.github.com>
2023-08-02 19:52:46 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,19 @@
-
+ def sum(a,b)
+ {
+	echo "the sum of ${a} and ${b} is ${a+b}"	
+ }
+ 
+ def sub(a,b)
+ {
+	echo "the substraction res of ${a} and ${b} is ${a-b}"	
+ }
+ 
+ def mul(a,b)
+ {
+	echo "the multipl res of ${a} and ${b} is ${a*b}"
+ }
+ 
+ def div(a,b)
+ {
+	echo "the div res of ${a} and ${b} is ${a/b}"
+ }
