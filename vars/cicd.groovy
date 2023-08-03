5161e8a7916230593a6484d5481ce747dd26ae6b
shashd1 <140515620+shashd1@users.noreply.github.com>
2023-08-03 20:11:38 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,3 +13,7 @@ def tomdeploy(jobname,ip,context)
 	sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
+def runselinium(jobname)
+{
+	sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
