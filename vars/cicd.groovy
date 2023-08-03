ff63508ea90476800ac7d6049bafd95a334f125d
shashd1 <140515620+shashd1@users.noreply.github.com>
2023-08-03 19:55:36 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,14 +8,3 @@ def mavenbuild()
 	sh 'mvn package'
 }
 
-def tomdeploy(Jobn,ip,context)
-{
-	sh "scp /var/lib/jenkins/workspace/${Jobn}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
-}
-def runSelinum(Jobn)
-{
-	sh "java -jar /var/lib/jenkins/workspace/${Jobn}/testing.jar"
-}
-
-
-
